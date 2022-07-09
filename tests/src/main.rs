fffd6895b28447433d78fa5974643bbfc6132616
Arpad Borsos <swatinem@swatinem.de>
2022-07-09 17:17:14 +0200

    add some more tests

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn some_fn(input: bool) -> usize {
+    if input {
+        2 + 4
+    } else {
+        3.saturating_add(5)
+    }
+}
+
+#[test]
+fn some_test() {
+    assert_eq!(some_fn(true), 6);
+    assert_eq!(some_fn(false), 8);
+}
