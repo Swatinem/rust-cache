9de8f90afbf8a9579dbb292c650653e5b7178ead
Arpad Borsos <swatinem@swatinem.de>
2023-07-31 16:34:21 +0200

    Add `jemalloc` to test `-sys` style dependencies

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,3 +1,10 @@
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 fn main() {
     println!("Hello, world!");
 }
