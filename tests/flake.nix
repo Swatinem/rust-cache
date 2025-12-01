6640fcbe8c237d2b4b872758b9161dc3eb57f760
marc0246 <40955683+marc0246@users.noreply.github.com>
2025-12-01 12:42:47 +0100

    Use the stable channel

--- tests/flake.nix
+++ tests/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
