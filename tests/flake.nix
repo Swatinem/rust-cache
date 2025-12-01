4bcebbedb5868f63ce37c231d0f616261fa10e06
marc0246 <40955683+marc0246@users.noreply.github.com>
2025-12-01 10:08:25 +0100

    Add a test

---
+++ tests/flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [ rust-bin.stable.latest.minimal ];
+        };
+      }
+    );
+}
+
